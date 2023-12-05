f4f745ba3b1b22f577c489cfae370200809d4da9
Alfuslog <148636926+Alfuslog@users.noreply.github.com>
2023-12-05 09:46:22 +0300

    new updated 7 laba

--- 7.py
+++ 7.py
@@ -16,23 +16,26 @@ def сntProbel(st):
             cnt += 1
     return cnt
 
+def genRandMatrix(nRows, nCols):
+    mat = []
+
+    for i in range(0, nRows):
+        row = [random.randrange(-10, 10) for _ in range(0, nCols)]
+
+        mat.append(row)
+
+    return mat
 
 def prog1():
     try:
-        #spis = []
-        #cnt = 0
-        #maxi = mini = 0
-        #st = str(input("введите строку, содержащую хотя бы один пробел:.."))
-        #for i in range(len(st)):
-        #    if st[i] == ' ':
-        #        spis.append(i)
-        #print(st[(spis[0]):(spis[-1])])
-        m = int(input("Введите количество столбцов матрицы"))
-        n = int(input("Введите количество строк матрицы"))
+        m = int(input("Введите количество столбцов матрицы:.."))
+        n = int(input("Введите количество строк матрицы:.."))
     except:
         oshibka()
-    k = 0
-    if (m and n) > 0:
+    kant = m
+    if (m and n) <= 0:
+        oshibka()
+    else:
         matrix = []
         for i in range(n):
             stroka = []
@@ -42,40 +45,68 @@ def prog1():
             matrix.append(stroka)
         for i in matrix:
             print(i)
-
-
-
-#def prog2():
-    #cnt = 0
-    #st = str(input("введите строку:.."))
-    #for i in st:
-    #    if i in chisla:
-    #        cnt += 1
-    #print(cnt)
+        for i in range(m):
+            for k in matrix:
+                if k[i] == 0:
+                    kant-=1
+                    break
+    print(f'количество ненулевых столбцов в матрице = {kant}')
 
 
 
 
+def prog2():
+    cnt = 0
+    summa = 0
+    try:
+        m = int(input("Введите количество столбцов матрицы:.."))
+        n = int(input("Введите количество строк матрицы:.."))
+    except:
+        oshibka()
+    kant = m
+    spisSum = []
+    if (m and n) <= 0:
+        oshibka()
+    else:
+        matrix = []
+        for i in range(n):
+            stroka = []
+            for k in range(m):
+                rand = random.randint(-10, 10)
+                stroka.append(rand)
+            matrix.append(stroka)
+        for i in matrix:
+            print(i)
+        for i in matrix:
+            for j in i:
+                summa += j
+            spisSum.append(summa)
+        print("~"*15)
+        print(spisSum, "суммы")
+        for i in range(len(spisSum) - 1):
+            for j in range(len(spisSum) - i - 1):
+                if spisSum[j] > spisSum[j + 1]:
+                    spisSum[j], spisSum[j + 1] = spisSum[j + 1], spisSum[j]
+                    matrix[j], matrix[j+1] = matrix[j+1], matrix[j]
+        print(spisSum, "последовательность сумм")
+        print("получившаяся матрица:")
+        for i in matrix:
+            print(i)
 
 
-#def prog3():
-#    try:
-#        stp = ''
-#        n = []
-#        ln = int(input("Введите длину списка:.."))
-#        l = [(random.randint(-100, 100) / (random.randint())) for i in range(ln)]
-#        min = l[0]
-#        print(l)
-#        for i in l:
-#            if min >= i:
-#                min = i
-#        print(min)
-#        for i in l:
-#            if abs(i) <= 1:
-#                l.insert(0, i)
-#
-#    except:
-#        oshibka()
+
+
+
+def prog3():
+    pythonist = 'pythonist'
+    slov = dict()
+    for i in pythonist:
+        cnt = 0
+        for j in pythonist:
+            if i==j:
+                cnt+=1
+        slov[i] = cnt
+    print(slov)
 
 
 CallProg = input("Введите номер задачи, который Вы хотите выполнить:..")
